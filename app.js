dfc034ad4ee1e11ef816e29c060699685eae8a90
00015272 <wiut00015272@gmail.com>
2022-03-31 19:12:16 +0500

    CRUD operations done

--- app.js
+++ app.js
@@ -15,8 +15,73 @@ app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'pug')
 
 // ROOT-URL
-app.get('/', function(req,res) {
-    res.render('index')
+app.get('/', async function(req,res) {
+    let workers = null;
+    try {
+        workers = await Worker.findAll()
+
+        let id = req.query.id != undefined ? req.query.id : undefined
+        let deleted = req.query.deleted == 'true' ? true : false
+        let updated = req.query.updated == 'true' ? true : false
+
+        res.render('index', {
+            workers: workers,
+            id: id,
+            deleted: deleted,
+            updated: updated
+        })
+
+    } catch {
+        workers= []
+        res.render('index', { workers: workers })
+    }
+})
+
+// GET : /CREATE
+app.get('/Create', (req, res) => {
+    res.render('create-update')
+})
+
+// POST : /CREATE-NEW-RECORD
+app.post('/create-new-record', async (req, res) => {
+    let worker = await Worker.create(req.body)
+    res.redirect(`/?id=${ worker.id }`)
+})
+
+// EDIT
+app.get('/edit/:id', async (req, res) => {
+    let id = req.params.id
+    let worker = await Worker.findByPk(id)
+    res.render('create-update', { worker: worker })
+})
+
+// UPDATE
+app.post('/update/:id', async (req, res) => {
+    let id = req.params.id
+    let result = await Worker.update({
+        fullName: req.body.fullName,
+        department: req.body.department,
+        bio: req.body.bio 
+    }, {
+        where: {
+            id: id
+        }
+    })
+
+    res.redirect('/?updated=true')
+})
+
+// DELETE
+app.get('/delete/:id', async (req, res) => {
+    let id = req.params.id
+
+    let result = await Worker.destroy({
+        where: {
+            id: id
+        }
+    })
+
+    res.redirect(`/?deleted=true`)
 })
 
 
